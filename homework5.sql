418938be305872807c1665f1f05aa00d16c44e6d
ercantncy <82731001+ercantncy@users.noreply.github.com>
2021-10-11 16:53:13 +0300

    Create homework5.sql

---
+++ homework5.sql
@@ -0,0 +1,8 @@
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+--select title,length from film where title like '%n' order by length desc limit 5
+
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci 5 filmi sıralayınız.
+--select title,length from film where title like '%n' order by length asc limit 5
+
+--customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+--select store_id,last_name from customer where store_id=1 order by last_name desc limit 4
